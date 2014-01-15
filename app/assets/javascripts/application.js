3951c20c61dbf4f015fc18b0b7a7701234fbe6bf
Samuel McTaggart <sam.mctaggart@gmail.com>
2014-01-15 10:46:52 -0500

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,29 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require_tree .
+
+$(function(){ $(document).foundation(); });
+
+$(document).ready(function(){
+  $('.pink').click(function(){
+    $('.seen').toggleClass('rotate');
+    $('.pink').mouseenter(function(){
+      $('.pink').text('Make it stop');
+      $('.pink').css({'position': 'absolute', 'top': Math.floor(Math.random() * $(document).height())+'px',
+                     'left': Math.floor(Math.random() * $(document).width())+'px'});
+    });
+  });
+});
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stylit::Application.config.session_store :cookie_store, key: '_stylit_session'
